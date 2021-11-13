576ae53f3e3fe59f7242102627001d0c5f124de9
Alvinditya Saputra <alvindityas@gmail.com>
2021-11-13 10:48:38 +0700

    add controller

---
+++ src/main/java/com/piinalpin/minio/http/controller/FileController.java
@@ -0,0 +1,45 @@
+package com.piinalpin.minio.http.controller;
+
+import com.piinalpin.minio.http.dto.FileDto;
+import com.piinalpin.minio.service.MinioService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import static org.springframework.web.servlet.HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/file")
+public class FileController {
+
+    @Autowired
+    private MinioService minioService;
+
+    @GetMapping
+    public ResponseEntity<Object> getFiles() {
+        return ResponseEntity.ok(minioService.getListObjects());
+    }
+
+    @GetMapping(value = "/**")
+    public ResponseEntity<Object> getFile(HttpServletRequest request) throws IOException {
+        String pattern = (String) request.getAttribute(BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String filename = new AntPathMatcher().extractPathWithinPattern(pattern, request.getServletPath());
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(IOUtils.toByteArray(minioService.getObject(filename)));
+    }
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<Object> upload(@ModelAttribute FileDto request) {
+        return ResponseEntity.ok().body(minioService.uploadFile(request));
+    }
+
+}
