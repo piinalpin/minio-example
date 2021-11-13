19e91d455c90c02b6f503cae263a02a8d7d3bbea
Alvinditya Saputra <alvindityas@gmail.com>
2021-11-13 10:48:09 +0700

    initial commit

---
+++ src/test/java/com/piinalpin/minio/MinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piinalpin.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MinioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
