be67f4f062669b164d6a22dea436cb84b7657657
Alvinditya Saputra <alvindityas@gmail.com>
2021-11-13 10:48:27 +0700

    add dto

---
+++ src/main/java/com/piinalpin/minio/http/dto/FileDto.java
@@ -0,0 +1,37 @@
+package com.piinalpin.minio.http.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FileDto implements Serializable {
+
+    private static final long serialVersionUID = 232836038145089522L;
+
+    private String title;
+
+    private String description;
+
+    @SuppressWarnings("java:S1948")
+    private MultipartFile file;
+
+    private String url;
+
+    private Long size;
+
+    private String filename;
+
+}
