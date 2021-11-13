c912162a6dd5d024c38b9a568f080ff38ac0b72f
Alvinditya Saputra <alvindityas@gmail.com>
2021-11-13 10:48:17 +0700

    add bean configuration

---
+++ src/main/java/com/piinalpin/minio/config/MinioConfiguration.java
@@ -0,0 +1,30 @@
+package com.piinalpin.minio.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class MinioConfiguration {
+
+    @Value("${minio.access.key}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String secretKey;
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Bean
+    @Primary
+    public MinioClient minioClient() {
+        return new MinioClient.Builder()
+                .credentials(accessKey, secretKey)
+                .endpoint(minioUrl)
+                .build();
+    }
+
+}
